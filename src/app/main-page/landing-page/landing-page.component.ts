a35a034da703a169f06e398979cba94fa2be8d34
Mihael0599 <mihaeldraz10@gmail.com>
2025-03-22 08:44:52 +0100

    fixed style for landing page

--- src/app/main-page/landing-page/landing-page.component.ts
+++ src/app/main-page/landing-page/landing-page.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { SlideTextComponent } from './slide-text/slide-text.component';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [],
+  imports: [SlideTextComponent],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss'
 })
---
+++ src/app/main-page/landing-page/slide-text/slide-text.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { CommonModule, NgFor, NgIf } from '@angular/common';
+
+@Component({
+  selector: 'app-slide-text',
+  standalone: true,
+  imports: [NgFor, NgIf],
+  templateUrl: './slide-text.component.html',
+  styleUrl: './slide-text.component.scss'
+})
+export class SlideTextComponent {
+
+  items: string[] = [
+    'Frontend Developer',
+    'Open to Work',
+    'Available for remote work',
+    'Based in Spaichingen'
+  ];
+
+}
