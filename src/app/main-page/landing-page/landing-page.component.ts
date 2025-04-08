c9ace38902e558dcf86e8fcd04092df6e53578a2
Mihael0599 <mihaeldraz10@gmail.com>
2025-04-08 12:09:19 +0200

    added englisch and german  json for translate

--- src/app/main-page/landing-page/landing-page.component.ts
+++ src/app/main-page/landing-page/landing-page.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { SlideTextComponent } from './slide-text/slide-text.component';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [SlideTextComponent],
+  imports: [SlideTextComponent, TranslateModule],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss'
 })
--- src/app/main-page/landing-page/slide-text/slide-text.component.ts
+++ src/app/main-page/landing-page/slide-text/slide-text.component.ts
@@ -1,20 +1,21 @@
 import { Component } from '@angular/core';
 import { CommonModule, NgFor, NgIf } from '@angular/common';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-slide-text',
   standalone: true,
-  imports: [NgFor, NgIf],
+  imports: [NgFor, NgIf, TranslateModule],
   templateUrl: './slide-text.component.html',
   styleUrl: './slide-text.component.scss'
 })
 export class SlideTextComponent {
 
   items: string[] = [
-    'Frontend Developer',
-    'Open to Work',
-    'Available for remote work',
-    'Based in Spaichingen',
+    'frontendDeveloper',
+    'openToWork',
+    'availableRemote',
+    'basedInSpaichingen',
   ];
 
 }
